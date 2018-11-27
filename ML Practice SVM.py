fffc03650dfd6923d0cbd35b9bc4c007bea21ffd
Burt <burtgwilliam@byu.net>
2018-11-27 11:13:21 -0500

    first commit

---
+++ ML Practice SVM.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+
+columns = ['id', 'clump_thickness', 'cell_size_uniformity', 'cell_shape_uniformity', 'marginal_adhesion', 'epithelial_cell_size', 'bare_nuclei', 'bland_chromatin', 'normal_nucleoli', 'mitosis', 'class']
+df = pd.read_csv('breast-cancer-wisconsin.data', header=None)
+df.columns = columns
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
+example_measures = example_measures.reshape(len(example_measures),-1)
+
+prediction = clf.predict(example_measures)
+print(prediction)
